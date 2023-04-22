c52f8dba1e2b0a6c17449f168e47bcfd0ab148df
Simon Lawliet <0x666666666@gmail.com>
2023-04-22 17:11:40 +0300

    zqwe

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 sudo apt update && apt upgrade
 if [["$1" == 'init']]; then
 	echo "Starting installation..."
-	sudo apt install -y python3, vim, curl, wget, npm, nodejs, tmux, php, gcc, code, build-essential, make > /dev/null
+	sudo apt install -y python3, vim, curl, wget, npm, nodejs, tmux, php, gcc, code, build-essential, make, google-chrome-stable > /dev/null
 	echo "Installed all packages, moving cfgs..."
 	mv .tmux.conf ~/
 	mv .vimrc ~/
