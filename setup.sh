58f202802faa9037fae99149eb32c372d7b77401
Simon Lawliet <0x666666666@gmail.com>
2023-04-22 16:46:22 +0300

    upd

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 sudo apt update
-sudo apt install python3, vim, curl, wget, npm, nodejs, tmux -y
+sudo apt install python3, vim, curl, wget, npm, nodejs, tmux, php, gcc, code, build-essential, make, git -y
