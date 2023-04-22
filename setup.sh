f99aa830811bc1be95ea39f5444fb590b7eae6a0
Simon Lawliet <0x666666666@gmail.com>
2023-04-22 16:51:44 +0300

    qwe

--- setup.sh
+++ setup.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-sudo apt update
+sudo apt update && apt upgrade
 sudo apt install python3, vim, curl, wget, npm, nodejs, tmux, php, gcc, code, build-essential, make, git -y
+mv .tmux.conf ~/
+mv .vimrc ~/
+echo "Done"
