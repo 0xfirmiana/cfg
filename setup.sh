b3c3e749790e3f49d289ea82ecd3211a59a78159
Simon Lawliet <0x666666666@gmail.com>
2023-04-22 18:06:48 +0300

    fix

--- setup.sh
+++ setup.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
-sudo apt update && apt upgrade
-if [["$1" == 'init']]; then
+if [[ "$1" == 'init' ]]; then
 	echo "Starting installation..."
-	sudo apt install -y python3, vim, curl, wget, npm, nodejs, tmux, php, gcc, code, build-essential, make, google-chrome-stable > /dev/null
+	apt install -y gh python3 vim curl wget npm nodejs tmux php gcc build-essential make > /dev/null
 	echo "Installed all packages, moving cfgs..."
-	mv .tmux.conf ~/
-	mv .vimrc ~/
+	cp .tmux.conf ~/
+	cp .vimrc ~/
 	echo "Done"
-else 
-	echo "Specify install type!"
+fi
