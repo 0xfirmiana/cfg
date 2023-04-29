722bdfecef882d1a09c3faa97a89714ac4e23dd8
Simon Lawliet <0x666666666@gmail.com>
2023-04-29 20:00:12 +0300

    qwe

--- setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-if [[ "$1" == 'init' ]]; then
-	echo "Starting installation..."
-	apt install -y gh python3 vim curl wget npm nodejs tmux php gcc build-essential make > /dev/null
-	echo "Installed all packages, moving cfgs..."
-	cp .tmux.conf ~/
-	cp .vimrc ~/
-	echo "Done"
-fi
